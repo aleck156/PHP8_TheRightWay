c43a9fd6a31ef9fbf5a56ed9b449e5281c3974ee
Aleksander Jaworski <aleck156@gmail.com>
2021-12-28 06:24:59 +0100

    completed constants and variable variables

--- basics/4_constants.php
+++ basics/4_constants.php
@@ -6,5 +6,30 @@ $firstName = 'Adam';
 
 echo $firstName;
 
-# defining constants
+# DEFINING CONSTANTS
+# defined at a runtime
 define('name', 'value'); // third parameter is deprecated, do not use it
+define('STATUS_PAID', 'paid');
+echo STATUS_PAID;
+
+echo defined('STATUS_PAID');
+
+# defined at a compile time
+const STATUS_VOID = 'void';
+
+if (true){
+  // const FOO = 'bar'; this won't work!
+  define('SOMETHING_NEW', 'value?!');
+}
+
+# built-in constants
+echo PHP_VERSION;
+
+# magic constants
+# their value can change depending where you use it
+# __<NAME>__ - dunders
+
+# variable variables
+$foo ='bar';
+$$foo = 'baz';
+echo "$foo, ${$foo}";

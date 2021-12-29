016b7d5a5ec0d992c3ebc9312ea5e9536a4080c2
Aleksander Jaworski <aleck156@gmail.com>
2021-12-29 08:39:49 +0100

    continue dat atypes video

---
+++ basics/5_dataTypes.php
@@ -0,0 +1,24 @@
+<?php
+/*
+# scalar types
+  bool
+  int
+  float
+  string
+# compound types
+  array
+  object
+  callable
+  iterable
+# special types
+  resource
+  null
+
+dynamic typic
+  type checking at runtime
+  php, js,
+static typing
+  type checking happens at compile time
+  java, c#, 
+
+*/
